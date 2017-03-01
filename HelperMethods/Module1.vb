0b7370d54c3ba9d80a329ec04c09349753493a4e
Joshua Marshall <jmarsh0614@gmail.com>
2017-02-28 23:05:32 -0500

    Add project files.

---
+++ HelperMethods/Module1.vb
@@ -0,0 +1,38 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Call displayTheMessage()
+        Console.ReadLine()
+        displayTheMessage()
+        Console.ReadLine()
+
+        Dim message As String
+        message = superSecretFormula()
+        Console.WriteLine(message)
+        Console.ReadLine()
+        Console.WriteLine(superSecretFormula())
+        Console.ReadLine()
+        Console.WriteLine(superSecretFormula("Bitch"))
+        Console.ReadLine()
+
+
+
+
+
+
+    End Sub
+    Sub displayTheMessage()
+        Console.WriteLine("From displayTheMessage")
+    End Sub
+
+    Function superSecretFormula() As String
+
+        Return "Hello World"
+    End Function
+
+    Function superSecretFormula(ByVal name As String) As String
+        Return String.Format("Hello {0}!", name)
+    End Function
+
+End Module
